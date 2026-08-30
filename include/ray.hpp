57f1846ebb09ac5b467a543e84ef603a45681daa
Ray Shealy <rbshealy227@gmail.com>
2026-08-29 21:29:41 -0500

    Add scaffolding for ray struct

---
+++ include/ray.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "vec3.hpp"
+
+/**
+ * @file ray.hpp
+ * @brief Ray 3D struct with essential operations for the raytracer
+ */
+namespace machc {
+    struct ray{
+        point3 orig;
+        vec3 dir;
+
+        ray() = default;
+        ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction){}
+
+        //Returns the point along the ray at time t
+        [[nodiscard]] point3 at(float t){
+            return orig + (t * dir);
+        }
+    
+    };
+}
