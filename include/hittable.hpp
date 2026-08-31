a3fd7e841ce2eb79cb00e8f30646f682e9a0544a
Ray Shealy <rbshealy227@gmail.com>
2026-08-30 19:14:28 -0500

    add HitRecord struct

---
+++ include/hittable.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "vec3.hpp"
+#include "ray.hpp"
+
+/**
+ * @file hittable.hpp
+ * @brief Hittable struct for objects that can be intersected by rays
+ */
+struct HitRecord {
+    float t;
+    machc::point3 p;
+    machc::vec3 normal;
+    uint32_t material_index;
+
+};
