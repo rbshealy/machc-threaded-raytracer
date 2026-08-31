482e60a3a1b71a344bf3f5d110a2f1e6d7fb84b2
Ray Shealy <rbshealy227@gmail.com>
2026-08-30 20:47:10 -0500

    Add machc namespace

--- include/sphere.hpp
+++ include/sphere.hpp
@@ -2,12 +2,14 @@
 #include "ray.hpp"
 #include "hittable.hpp"
 
-struct Sphere {
-    machc::point3 center;
-    float radius;
-    uint32_t material_index;
+namespace machc {
+    struct Sphere {
+        point3 center;
+        float radius;
+        uint32_t material_index;
 
-    Sphere() : center(machc::point3(0, 0, 0)), radius(1.0f), material_index(0) {}
-    Sphere(const machc::point3& center, float radius, uint32_t material_index) : center(center), radius(radius), material_index(material_index) {}
+        Sphere() : center(point3(0, 0, 0)), radius(1.0f), material_index(0) {}
+        Sphere(const point3& center, float radius, uint32_t material_index) : center(center), radius(radius), material_index(material_index) {}
 
-};
+    };
+}
