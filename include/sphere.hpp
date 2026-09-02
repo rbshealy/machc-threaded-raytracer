e8c97867606b84873f223b48f250ea45dbfbd95a
Ray Shealy <rbshealy227@gmail.com>
2026-09-02 17:07:25 -0500

    Add sphere and triangle geometry

--- include/sphere.hpp
+++ include/sphere.hpp
@@ -11,5 +11,36 @@ namespace machc {
         Sphere() : center(point3(0, 0, 0)), radius(1.0f), material_index(0) {}
         Sphere(const point3& center, float radius, uint32_t material_index) : center(center), radius(radius), material_index(material_index) {}
 
+        bool hit (const ray& r, float t_min, float t_max, HitRecord& rec){
+            vec3 oc = r.orig - center;
+
+            float a = r.dir.length_squared();
+            float bdiv2 = oc.dot(r.dir);
+            float c = oc.length_squared() - (radius * radius);
+
+            float discrim = (bdiv2 * bdiv2) - (a * c); // using reduced discriminant to not carry factor of 2 for the entire quadratic
+
+            if (discrim < 0.0f){
+                return false;
+            }
+
+            float t = (-bdiv2 - std::sqrt(discrim)) / a; // because of the math trick we did the entire quadratic is simpiler and can drop extra multiplies
+
+            if (t < t_min || t > t_max){
+                t = (-bdiv2 + std::sqrt(discrim)) / a;
+
+                if (t < t_min || t > t_max){
+                    return false;
+                }
+            }
+
+            rec.t = t;
+            rec.p = r.at(t);
+            rec.material_index = material_index;
+            rec.calc_normal(r, (rec.p - center) / radius);
+
+
+            return true;
+        }
     };
 }
--- include/triangle.hpp
+++ include/triangle.hpp
@@ -1,7 +1,12 @@
 #pragma once
 #include "vec3.hpp"
+#include "ray.hpp"
+#include "hittable.hpp"
 
-
+/**
+ * @file triangle.hpp
+ * @brief Triangle struct with hit method to calculate ray intersection
+ */
 namespace machc {
     struct Triangle {
         point3 v0;
@@ -12,5 +17,50 @@ namespace machc {
         Triangle() : v0(point3(0, 0, 0)), v1(point3(1, 0, 0)), v2(point3(0, 1, 0)), material_index(0) {}
         Triangle(const point3& v0, const point3& v1, const point3& v2, uint32_t material_index) : v0(v0), v1(v1), v2(v2), material_index(material_index) {}
 
+        // can be done by solving a 3x3 system but Moller-Trumbore algorithm is more efficient
+        bool hit(const ray& r, float t_min, float t_max, HitRecord& rec) {
+            float epsilon = 1e-6f; //Moller-Trumbore recommends epsilon of 1e-6f, may need to adjust later for shadow acne or if objects are far from the origin
+
+            vec3 edge1 = v1 - v0;
+            vec3 edge2 = v2 - v0;
+
+            vec3 dXedge2 = r.dir.cross(edge2);
+            float area = edge1.dot(dXedge2); //get area of paralleliped formed by edge1, edge2, and ray dir (scalar triple product)
+            // will reuse this later for speed instead of calculating other denomenators
+
+            if (std::abs(area) < epsilon){
+                return false; // if 3 vectors are coplanar we throw them out, either the ray is on the triangle or parallel
+            }
+
+            float inv_area = 1.0f / area;
+
+            vec3 T = r.orig - v0;
+            float u = T.dot(dXedge2) * inv_area;
+
+            if (u < 0.0 || u > 1.0){
+                return false; //if u does not land between v0 and v1 we throw it out
+            }
+
+            vec3 tXedge1 = T.cross(edge1);
+            float v = r.dir.dot(tXedge1) * inv_area;
+
+            if (v < 0.0 || v + u > 1.0){
+                return false; // if v and u greater than 1 then we are off the triangle, if v < 0 we are off the triangle
+            }
+        
+            float t = edge2.dot(tXedge1) * inv_area;
+            if (t < t_min || t > t_max){
+                return false; // handling if the hit is behind the camera (t_min) or we've already found one closer (t_max)
+            }
+
+            rec.t = t;
+            rec.p = r.at(t);
+            rec.material_index = material_index;
+            rec.calc_normal(r, edge1.cross(edge2).normalized());
+            
+            return true;
+        }
     };
+
+    
 }
